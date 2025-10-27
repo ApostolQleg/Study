ae8f809dd12c788becf5e56962585ea9c358d2ee
ApostolOleg <apostololeg0@gmail.com>
2025-10-27 10:33:28 +0200

    Pre-alpha 3

--- JS/projects/project/script.js
+++ JS/projects/project/script.js
@@ -4,5 +4,43 @@ player.style.display = "none";
 
 startButton.addEventListener("click", () => {
 	startButton.style.display = "none";
+	player.style.top = "50%";
+	player.style.left = "50%";
 	player.style.display = "block";
 });
+
+// Movement logic (WASD) for the player (will be changed a lot)
+const keysPressed = {};
+const step = 3;
+let x = window.innerWidth / 2 - player.offsetWidth / 2; // Centered x position
+let y = window.innerHeight / 2 - player.offsetHeight / 2; // Centered y position
+
+document.addEventListener("keydown", (event) => {
+	keysPressed[event.code] = true;
+});
+
+document.addEventListener("keyup", (event) => {
+	keysPressed[event.code] = false;
+});
+
+function movement() {
+	if (keysPressed["KeyW"]) {
+		y -= step;
+	}
+	if (keysPressed["KeyA"]) {
+		x -= step;
+	}
+	if (keysPressed["KeyS"]) {
+		y += step;
+	}
+	if (keysPressed["KeyD"]) {
+		x += step;
+	}
+
+	player.style.top = y + "px";
+	player.style.left = x + "px";
+
+	requestAnimationFrame(movement);
+}
+
+requestAnimationFrame(movement);
