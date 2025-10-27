392d940935a9da62ce010366b496986080b19e4d
ApostolOleg <apostololeg0@gmail.com>
2025-10-27 11:23:19 +0200

    Pre-alpha 4

--- JS/projects/project/script.js
+++ JS/projects/project/script.js
@@ -11,9 +11,9 @@ startButton.addEventListener("click", () => {
 
 // Movement logic (WASD) for the player (will be changed a lot)
 const keysPressed = {};
-const step = 3;
-let x = window.innerWidth / 2 - player.offsetWidth / 2; // Centered x position
-let y = window.innerHeight / 2 - player.offsetHeight / 2; // Centered y position
+const step = 10;
+let x = window.innerWidth / 2; // Centered x position
+let y = window.innerHeight / 2; // Centered y position
 
 document.addEventListener("keydown", (event) => {
 	keysPressed[event.code] = true;
@@ -25,20 +25,33 @@ document.addEventListener("keyup", (event) => {
 
 function movement() {
 	if (keysPressed["KeyW"]) {
-		y -= step;
-	}
-	if (keysPressed["KeyA"]) {
-		x -= step;
-	}
-	if (keysPressed["KeyS"]) {
 		y += step;
 	}
-	if (keysPressed["KeyD"]) {
+	if (keysPressed["KeyA"]) {
 		x += step;
 	}
+	if (keysPressed["KeyS"]) {
+		y -= step;
+	}
+	if (keysPressed["KeyD"]) {
+		x -= step;
+	}
 
-	player.style.top = y + "px";
-	player.style.left = x + "px";
+	background.style.top = y + "px";
+	background.style.left = x + "px";
+
+	if (x > window.innerWidth) {
+		x = 0;
+	}
+	if (x < 0) {
+		x = window.innerWidth;
+	}
+	if (y > window.innerHeight) {
+		y = 0;
+	}
+	if (y < 0) {
+		y = window.innerHeight;
+	}
 
 	requestAnimationFrame(movement);
 }
